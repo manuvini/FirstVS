d88e5193e6bb9c2e4842f8342ad476833f146e48
Manoj <manuvini29@gmail.com>
2023-11-24 06:44:15 +0000

    one more commit

--- force-app/main/default/lwc/accountsearchresults/accountsearchresults.js
+++ force-app/main/default/lwc/accountsearchresults/accountsearchresults.js
@@ -4,7 +4,7 @@ import { LightningElement, api } from 'lwc';
 const columns  = [
     {label: 'Account Id ', fieldName: 'id'},
     {label: 'Account Name', fieldName: 'name'}
-]
+] 
 
 export default class Accountsearchresults extends LightningElement {
 
